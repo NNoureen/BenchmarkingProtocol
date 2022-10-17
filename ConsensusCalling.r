2596493edbab91444cb71581f0aa713dd4ee9061
NNoureen <56266048+NNoureen@users.noreply.github.com>
2022-10-17 15:47:59 -0500

    Update ConsensusCalling.r

--- ConsensusCalling.r
+++ ConsensusCalling.r
@@ -22,20 +22,20 @@ write.table(Res,'CommonCases_ES_IDHAstrocytoma_17March2021.txt',sep='\t',quote=F
 Sensitivityfunc <- function(Data){
 CommonCases = rbind()
 
-Case1 = Data[which((Data$ES_AUCell >= 1 & Data$ES_SCSE >= 1 & Data$ES_JAS_NM < 1)),]
+Case1 = Data[which((Data$ES_AUCell >= 1 & Data$ES_SCSE >= 1 & Data$ES_JAS < 1)),]
 
 subsetcommon = data.frame(nrow(Case1),'AUCell vs SCSE','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
-Case2 = Data[which((Data$ES_AUCell >= 1 & Data$ES_JAS_NM >= 1 & Data$ES_SCSE < 1)),]
+Case2 = Data[which((Data$ES_AUCell >= 1 & Data$ES_JAS >= 1 & Data$ES_SCSE < 1)),]
 
 subsetcommon = data.frame(nrow(Case2),'AUCell vs JAS','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
-Case3 = Data[which((Data$ES_SCSE >= 1 & Data$ES_JAS_NM >= 1 & Data$ES_AUCell < 1)),]
+Case3 = Data[which((Data$ES_SCSE >= 1 & Data$ES_JAS >= 1 & Data$ES_AUCell < 1)),]
 
 subsetcommon = data.frame(nrow(Case3),'SCSE vs JAS','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
@@ -43,7 +43,7 @@ CommonCases = rbind(CommonCases,subsetcommon)
 
 
 
-Case4 = Data[which((Data$ES_AUCell <= -1 & Data$ES_SCSE <= -1 & Data$ES_JAS_NM > -1)),]
+Case4 = Data[which((Data$ES_AUCell <= -1 & Data$ES_SCSE <= -1 & Data$ES_JAS > -1)),]
 
 subsetcommon = data.frame(nrow(Case4),'AUCell vs SCSE','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
@@ -51,62 +51,62 @@ CommonCases = rbind(CommonCases,subsetcommon)
 
 
 
-Case5 = Data[which((Data$ES_AUCell <= -1 & Data$ES_JAS_NM <= -1 & Data$ES_SCSE > -1)),]
+Case5 = Data[which((Data$ES_AUCell <= -1 & Data$ES_JAS <= -1 & Data$ES_SCSE > -1)),]
 
 subsetcommon = data.frame(nrow(Case5),'AUCell vs JAS','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
-Case6 = Data[which((Data$ES_SCSE <= -1 & Data$ES_JAS_NM <= -1 & Data$ES_AUCell > -1)),]
+Case6 = Data[which((Data$ES_SCSE <= -1 & Data$ES_JAS <= -1 & Data$ES_AUCell > -1)),]
 
 subsetcommon = data.frame(nrow(Case6),'SCSE vs JAS','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
-Case7 = Data[which(Data$ES_AUCell >= 1 & Data$ES_SCSE < 1 & Data$ES_JAS_NM < 1),]
+Case7 = Data[which(Data$ES_AUCell >= 1 & Data$ES_SCSE < 1 & Data$ES_JAS < 1),]
 subsetcommon = data.frame(nrow(Case7),'AUCell','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
-Case8 = Data[which(Data$ES_SCSE >= 1 & Data$ES_JAS_NM < 1 & Data$ES_AUCell < 1),]
+Case8 = Data[which(Data$ES_SCSE >= 1 & Data$ES_JAS < 1 & Data$ES_AUCell < 1),]
 subsetcommon = data.frame(nrow(Case8),'SCSE','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
 
-Case9 = Data[which(Data$ES_JAS_NM >= 1 & Data$ES_SCSE < 1 & Data$ES_AUCell < 1),]
+Case9 = Data[which(Data$ES_JAS >= 1 & Data$ES_SCSE < 1 & Data$ES_AUCell < 1),]
 subsetcommon = data.frame(nrow(Case9),'JAS','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
 
-Case10 = Data[which(Data$ES_AUCell <= -1 & Data$ES_SCSE > -1 & Data$ES_JAS_NM > -1),]
+Case10 = Data[which(Data$ES_AUCell <= -1 & Data$ES_SCSE > -1 & Data$ES_JAS > -1),]
 subsetcommon = data.frame(nrow(Case10),'AUCell','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
-Case11 = Data[which(Data$ES_SCSE <= -1 & Data$ES_JAS_NM > -1 & Data$ES_AUCell > -1),]
+Case11 = Data[which(Data$ES_SCSE <= -1 & Data$ES_JAS > -1 & Data$ES_AUCell > -1),]
 subsetcommon = data.frame(nrow(Case11),'SCSE','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
-Case12 = Data[which(Data$ES_JAS_NM <= -1 & Data$ES_SCSE > -1 & Data$ES_AUCell > -1),]
+Case12 = Data[which(Data$ES_JAS <= -1 & Data$ES_SCSE > -1 & Data$ES_AUCell > -1),]
 subsetcommon = data.frame(nrow(Case12),'JAS','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
 
-Case13 = Data[which(Data$ES_JAS_NM >= 1 & Data$ES_SCSE >= 1 & Data$ES_AUCell >= 1),]
+Case13 = Data[which(Data$ES_JAS >= 1 & Data$ES_SCSE >= 1 & Data$ES_AUCell >= 1),]
 subsetcommon = data.frame(nrow(Case13),'AUCell & SCSE & AUCell','Pos')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
 
-Case14 = Data[which(Data$ES_JAS_NM <= -1 & Data$ES_SCSE <= -1 & Data$ES_AUCell <= -1),]
+Case14 = Data[which(Data$ES_JAS <= -1 & Data$ES_SCSE <= -1 & Data$ES_AUCell <= -1),]
 subsetcommon = data.frame(nrow(Case14),'AUCell & SCSE & AUCell','Neg')
 colnames(subsetcommon) = c('Common','Methods','ESDir')
 CommonCases = rbind(CommonCases,subsetcommon)
